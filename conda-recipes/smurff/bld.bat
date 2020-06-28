53d01c5ce9f5f5e3cb00f58fd5f03e1b7567fa97
Tom Vander Aa <vanderaa@imec.be>
2020-06-28 13:10:01 +0200

    WIP: conda build on azure pipelines

--- conda-recipes/smurff/bld.bat
+++ conda-recipes/smurff/bld.bat
@@ -1,6 +1,5 @@
 REM SET "CMAKE_ARGS=-DENABLE_MKL=ON -DBoost_COMPILER=-vc140 -DCMAKE_INSTALL_PREFIX=%PREFIX%"
 SET "CMAKE_ARGS=-DENABLE_MKL=ON -DCMAKE_INSTALL_PREFIX=%PREFIX%"
-SET "BUILD_ARGS=-j"
 
 REM conda build sets "Visual Studio 15 2017 Win64" generator
 REM which is incompatible with "-A x64" that we add ourselves in setup.py,
