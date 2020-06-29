42872c04c9e234672b54c1e9387951bf2f23a085
Tom Vander Aa <vanderaa@imec.be>
2020-06-29 13:55:46 +0200

    WIP: macos build script

--- ci/buildwheel/macos.sh
+++ ci/buildwheel/macos.sh
@@ -3,7 +3,7 @@
 set -e
 
 brew update
-for BREW_PKG in pyenv openblas 
+for BREW_PKG in pyenv eigen lapack pybind11 ci/highfive.rb
 do
     brew outdated $BREW_PKG || brew upgrade $BREW_PKG
 done
@@ -11,13 +11,13 @@ done
 
 export MACOSX_DEPLOYMENT_TARGET=10.9
 export PATH=~/.pyenv/shims:$PATH
-CMAKE_ARGS="-DCMAKE_PREFIX_PATH=/usr/local/opt/openblas -DENABLE_OPENBLAS=ON -DENABLE_BOOST=OFF "
+export CMAKE_ARGS="-DCMAKE_PREFIX_PATH=/usr/local/opt/lapack -DENABLE_BOOST=OFF -DENABLE_CMDLINE=OFF -DENABLE_TESTS=OFF"
 
 for PYVER in "3.5.9" "3.6.10" "3.7.7" "3.8.2"; do
   pyenv install --skip-existing ${PYVER}
   pyenv global ${PYVER}
   python -m pip install Cython wheel numpy delocate
-  python setup.py --extra-cmake-args "$CMAKE_ARGS" bdist_wheel
+  python setup.py bdist_wheel
 done
 
 mkdir -p fixed_wheels
