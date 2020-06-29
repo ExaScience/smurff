8c011eb818395476884ed8d1ea06cfb2e2bf34f6
Tom Vander Aa <vanderaa@imec.be>
2020-06-29 13:26:32 +0200

    ci: add test requirements

--- ci/buildwheel/linux.sh
+++ ci/buildwheel/linux.sh
@@ -3,6 +3,7 @@
 export CIBW_BUILD='cp3*-manylinux_x86_64'
 export CIBW_ENVIRONMENT='CMAKE_ARGS="-DENABLE_BOOST=OFF -DENABLE_CMDLINE=OFF -DENABLE_TESTS=OFF" BUILD_ARGS=-j'
 export CIBW_MANYLINUX_X86_64_IMAGE=vanderaa/manylinux2014_x86_64_smurff
-export CIBW_TEST_COMMAND='python -m unittest discover {project}/python/test'
+export CIBW_TEST_COMMAND='pytest {project}/python/test'
+export CIBW_TEST_REQUIRES='parameterized pytest'
 
 cibuildwheel --platform linux .
