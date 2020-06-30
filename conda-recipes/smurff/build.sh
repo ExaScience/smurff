0ca8d0f3d79bdefc93b1ec82f52c2ab1e076b6bc
Tom Vander Aa <vanderaa@imec.be>
2020-06-30 14:52:42 +0200

    ENH: cleanup conda build.sh script

--- conda-recipes/smurff/build.sh
+++ conda-recipes/smurff/build.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 
-if [ -n "$OSX_ARCH" ]
-then
-    OPENMP_FLAGS="-DOpenMP_CXX_FLAG=-fopenmp=libiomp5 -DOpenMP_C_FLAG=-fopenmp=libiomp5"
-else
-    OPENMP_FLAGS=""
-fi
-
-CMAKE_ARGS="-DENABLE_MKL=ON $OPENMP_FLAGS -DCMAKE_INSTALL_PREFIX=$PREFIX -DENABLE_MPI=OFF"
-BUILD_ARGS="-j$CPU_COUNT"
+export CMAKE_ARGS="-DENABLE_MKL=ON -DCMAKE_INSTALL_PREFIX=$PREFIX -DENABLE_MPI=OFF -DENABLE_ASAN=ON -DCMAKE_BUILD_TYPE=RelWithDebInfo"
 
 $PYTHON setup.py install \
     --install-binaries \
-    --extra-cmake-args "$CMAKE_ARGS" \
-    --extra-build-args "$BUILD_ARGS" \
     --single-version-externally-managed --record=record.txt
