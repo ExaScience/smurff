38d4b6fb8afdb7efdfb68ae5d20f58633702a0dc
Tom Vander Aa <vanderaa@imec.be>
2020-07-02 14:47:12 +0200

    WIP: upgrading Dockerfiles to pytest

--- ci/ubuntu1910/build_script.sh
+++ ci/ubuntu1910/build_script.sh
@@ -2,7 +2,7 @@
 #
 # Start this script in a Docker container like this:
 #
-#  docker run -eCPU_COUNT=2 -v $(git rev-parse --show-toplevel):/smurff -ti smurff1910 /smurff/ci/ubuntu1910/build_script.sh
+#  docker run -eCPU_COUNT=2 -v $(git rev-parse --show-toplevel):/smurff -ti smurff1910 /smurff/ci/ubuntu1910/work_script.sh
 #
 # where smurff1910 is the image name
 
@@ -10,9 +10,8 @@
 set -e
 set -x
 
-rm -rf /build  && mkdir /build && cd /build
-git clone /smurff 
-cd smurff/python/smurff
-python3 setup.py install --install-binaries --extra-build-args "-j ${CPU_COUNT}"
+rm -rf /work  && mkdir /work && cd /work
+git clone /smurff && cd smurff
+python3 setup.py install --install-binaries
 /usr/local/libexec/tests
-python3 -m unittest discover test
+pytest-3 python/test
