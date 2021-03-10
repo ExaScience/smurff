c0a9fa88cfeca5de68b97e596a487086131bf6fd
Tom Vander Aa <vanderaa@imec.be>
2021-03-10 11:41:15 +0100

    Generate features matrix for movielens

--- data/movielens/preprocess_movielens.py
+++ data/movielens/preprocess_movielens.py
@@ -1,21 +1,38 @@
 #!/usr/bin/python 
 
+from pprint import pprint
 import pandas as pd
 import numpy as np
+from sklearn.feature_extraction import DictVectorizer
 
 movies = pd.read_csv("merged.csv")
+movies["genres_x"] = movies["genres_x"].str.replace("|", ",", regex=False)
+movies["timestamp"] = pd.to_datetime(movies["date"]).fillna(pd.Timestamp(0.)).astype('int64') // 10**9
 
-genres = pd.DataFrame([ genre.split("|") for genre in movies["genres_x"] ])
-print(f"genres: {genres.shape}")
+numerical_columns = [
+    "timestamp",
+    "runtime",
+    "revenue",
+    "budget",
+    "popularity",
+    "average_vote",
+    "num_votes",
+]
 
-flat = genres.values.flatten()
-flat = np.delete(flat, np.where(flat == None))
-unique = np.unique(flat)
-print(f"unique genres: {len(np.unique(flat))}")
+category_columns = [
+    "genres_x",
+    "genres_y",
+    #"keywords",
+    #"director",
+    #"cast",
+    #"production_companies",
+    "production_countries",
+    "language",
+]
 
+for col in category_columns:
+    movies[col] = movies[col].str.split(", ?")
 
-# 	movieId	title_x	genres_x	imdbId	tmdbId	id	title_y	tagline	description	genres_y	keywords	date	collection	runtime	revenue	budget	director	cast	production_companies	production_countries	popularity	average_vote	num_votes	language	imdb_id	poster_url
-# 	movieId	title_x	genres_x	imdbId	tmdbId	id	title_y	tagline	description	genres_y	keywords	date	collection	runtime	revenue	budget	director	cast	production_companies	production_countries	popularity	average_vote	num_votes	language	imdb_id	poster_url
-# 
-# 
-# 0	1	Toy Story (1995)	Adventure|Animation|Children|Comedy|Fantasy	114709	862	862	Toy Story		Led by Woody, Andy's toys live happily in his room until Andy's birthday brings Buzz Lightyear onto the scene. Afraid of losing his place in Andy's heart, Woody plots against Buzz. But when circumstances separate Buzz and Woody from their owner, the duo eventually learns to put aside their differences.	animation, comedy, family	jealousy, toy, boy, friendship, friends, rivalry, boy next door, new toy, toy comes to life	30/10/1995	Toy Story Collection	81	373554033	30000000	John Lasseter	Tom Hanks, Tim Allen, Don Rickles, Jim Varney, Wallace Shawn, John Ratzenberger, Annie Potts, John Morris, Erik von Detten, Laurie Metcalf, R. Lee Ermey, Sarah Freeman, Penn Jillette	Pixar Animation Studios	United States of America	21.946943	7.7	5415	en	tt0114709	/rhIRbceoE9lR4veEXuwCC2wARtG.jpg
+selected = movies[numerical_columns + category_columns].to_dict(orient = 'records')
+vec = DictVectorizer()
+features = vec.fit_transform(selected)
