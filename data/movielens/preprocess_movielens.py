5da1cc88c68ef8f22e4004adc7fa24d5aad8f7cb
Tom Vander Aa <vanderaa@imec.be>
2021-03-10 16:10:56 +0100

    Pickled preprocessed movielens features

--- data/movielens/preprocess_movielens.py
+++ data/movielens/preprocess_movielens.py
@@ -3,9 +3,20 @@
 from pprint import pprint
 import pandas as pd
 import numpy as np
+import scipy.sparse as sp
 from sklearn.feature_extraction import DictVectorizer
 
 movies = pd.read_csv("merged.csv")
+movies["movieIdx"] = movies.index # used in sparse matrix
+
+ratings = pd.read_csv("ratings.csv")
+
+ratings["userIdx"] = ratings["userId"] - 1
+ratings = pd.merge(ratings, movies, on = "movieId", how = "inner")
+ratings_matrix = sp.coo_matrix((ratings['rating'].values,
+        (ratings['movieIdx'].values, ratings['userIdx'].values)))
+
+
 movies["genres_x"] = movies["genres_x"].str.replace("|", ",", regex=False)
 movies["timestamp"] = pd.to_datetime(movies["date"]).fillna(pd.Timestamp(0.)).astype('int64') // 10**9
 
@@ -34,5 +45,11 @@ for col in category_columns:
     movies[col] = movies[col].str.split(", ?")
 
 selected = movies[numerical_columns + category_columns].to_dict(orient = 'records')
-vec = DictVectorizer()
-features = vec.fit_transform(selected)
+features_vectorizer = DictVectorizer()
+features_matrix = features_vectorizer.fit_transform(selected)
+
+import pickle
+
+pickle.dump(features_vectorizer, open("movies_features_vectorizer.pickle", "wb"))
+pickle.dump(features_matrix,     open("movie_features_matrix.pickle", "wb"))
+pickle.dump(ratings_matrix,      open("ratings_matrix.pickle", "wb"))
