9ab30f134b8dc46e372a8bc97fbee42e02a08edf
Tom Vander Aa <vanderaa@imec.be>
2021-03-11 16:43:03 +0100

    WIP: movielens predict

--- data/movielens/test_py.py
+++ data/movielens/test_py.py
@@ -7,11 +7,10 @@ Y = smurff.matrix_io.read_matrix("ratings_1k_random.sdm")
 Ytrain, Ytest = smurff.prepare.make_train_test(Y, 0.2)
 sideinfo = smurff.matrix_io.read_matrix("features_1k_random.sdm")
 
-result = smurff.macau(Ytrain = Ytrain, Ytest = Ytest, side_info = [sideinfo, None], verbose = 1, num_latent = 16)
-result = smurff.bpmf(Ytrain = Ytrain, Ytest = Ytest, verbose = 1, num_latent = 16)
 
-# trainSession = smurff.TrainSession(num_latent=8, verbose=2)
-# trainSession.addTrainAndTest(Ytrain, Ytest)
-# trainSession.addSideInfo(0, sideinfo, smurff.FixedNoise(10.))
-# 
-# trainSession.run()
+trainSession = smurff.TrainSession(num_latent=8, burnin = 200, nsamples = 200, verbose=1, save_name = "movielens.hdf5", save_freq = 1)
+trainSession.addTrainAndTest(Ytrain, Ytest)
+trainSession.addSideInfo(0, sideinfo, smurff.FixedNoise(10.))
+
+trainSession.run()
+
