634a49f3099a83777a6ca2291c8a4af346269df6
Tom Vander Aa <vanderaa@imec.be>
2021-03-12 15:51:10 +0100

    WIP: test_predict

--- python/test/test_predict.py
+++ python/test/test_predict.py
@@ -1,10 +1,11 @@
+import itertools
 import unittest
 from parameterized import parameterized
 import numpy as np
 from numpy.testing import assert_almost_equal
 import pandas as pd
-import scipy.sparse
 import smurff
+from pprint import pprint
 
 verbose = 0
 
@@ -12,11 +13,10 @@ class TestPredictSession(unittest.TestCase):
     # Python 2.7 @unittest.skip fix
     __name__ = "TestPredictSession"
 
-    def run_train_session(self, nmodes, sparse):
-        shape = range(2, nmodes+2) # 2, 3, 4, ... 
-        Y = np.random.rand(*shape)
-        if sparse: # make Y SparseTensor through make_train_test
-            _, Y = smurff.make_train_test(Y, 0.5)
+
+    def run_train_session(self, nmodes, density):
+        shape = range(11, nmodes+11) # 11, 12, 13, ... 
+        Y, X = smurff.generate.gen_tensor(shape, 3, density)
         self.Ytrain, self.Ytest = smurff.make_train_test(Y, 0.1)
         priors = ['normal'] * nmodes
 
@@ -25,17 +25,22 @@ class TestPredictSession(unittest.TestCase):
                 save_freq = 1, save_name = smurff.helper.temp_savename())
 
         trainSession.addTrainAndTest(self.Ytrain, self.Ytest)
+        for i, x in enumerate(X):
+            trainSession.addSideInfo(i, x)
 
         trainSession.init()
         while trainSession.step():
             pass
 
-        return trainSession
+        return trainSession, Y, X
 
-    def run_predict_session(self, nmodes, sparse):
-        train_session = self.run_train_session(nmodes, sparse)
+    def run_predict_session(self, nmodes, density):
+        train_session, Y, X = self.run_train_session(nmodes, density)
         predict_session = train_session.makePredictSession()
+        self.run_predict_some_all_one(train_session, predict_session)
+        self.run_predict_predict(predict_session, X)
 
+    def run_predict_some_all_one(self, train_session, predict_session):
         p1 = sorted(train_session.getTestPredictions())
         p2 = sorted(predict_session.predict_some(self.Ytest))
         p3 = predict_session.predict_one(p1[0].coords, p1[0].val)
@@ -67,13 +72,46 @@ class TestPredictSession(unittest.TestCase):
         self.assertAlmostEqual(train_session.getRmseAvg(), p2_rmse_avg, places = 2)
         self.assertAlmostEqual(train_session.getRmseAvg(), p1_rmse_avg, places = 2)
 
-    @parameterized.expand(map(lambda x: ("dims%d" % x, x), range(2,7))) # 2, 3, ..., 6
-    def test_predict_dense(self, name, nmodes):
-        self.run_predict_session(nmodes, False)
+    def run_predict_predict(self, predict_session, X):
+        """ Test the PredictSession.predict function """
+
+        def run_n_samples(samples, expected_nsamples):
+            operand_and_sizes = [ 
+                [
+                    ( slice(10)  , 10         ),
+                    ( range(10)  , 10         ),
+                    ( 10         , 1          ),
+                    ( x          , x.shape[0] ),
+                    ( x[10]      , 1          ),
+                    ( x[8:11]    , 3          ),
+                    ( x[2:11:2]  , 5          ),
+                ]
+            for x in X ]
+
+            for o in itertools.product(*operand_and_sizes):
+                operands, expected_sizes = zip(*o) # unzip
+                expected_shape = (expected_nsamples,) + expected_sizes
+
+                print("================")
+                print("operands = ", operands)
+                print("expected_sizes = ", expected_shape)
+                shape = predict_session.predict(operands, samples).shape
+                print("got = ", shape)
+                self.assertEqual(shape, expected_shape)
+
+        run_n_samples(None, len(predict_session.samples))
+        run_n_samples(slice(10), 10)
+        run_n_samples(slice(10, 20), 10)
+        run_n_samples(slice(10, 20, 2), 5)
+
 
     @parameterized.expand(map(lambda x: ("dims%d" % x, x), range(2,7))) # 2, 3, ..., 6
-    def test_predict_sparse(self, name, nmodes):
-        self.run_predict_session(nmodes, True)
+    def test_predict_dense(self, name, nmodes):
+        self.run_predict_session(nmodes, 1.0)
+
+    @parameterized.expand(map(lambda x: ("dims%d" % x, x), range(2,7))) # 2, 3, ..., 6
+    def test_predict_density(self, name, nmodes):
+        self.run_predict_session(nmodes, 0.5)
 
 if __name__ == '__main__':
     unittest.main()
