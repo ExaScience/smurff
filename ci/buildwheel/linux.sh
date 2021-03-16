999d77dc93d22a987a15c82fa093a79e17448176
Tom Vander Aa <vanderaa@imec.be>
2021-03-16 08:44:08 +0100

    FIX: skip python 3.5

--- ci/buildwheel/linux.sh
+++ ci/buildwheel/linux.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export CIBW_BUILD='cp3*-manylinux_x86_64'
+export CIBW_BUILD='cp36-manylinux_x86_64 cp37-manylinux_x86_64 cp38-manylinux_x86_64 cp39-manylinux_x86_64'
 export CIBW_ENVIRONMENT='CMAKE_ARGS="-DENABLE_BOOST=OFF -DENABLE_CMDLINE=OFF -DENABLE_TESTS=OFF" BUILD_ARGS=-j'
 export CIBW_MANYLINUX_X86_64_IMAGE=vanderaa/manylinux2014_x86_64_smurff
 export CIBW_TEST_COMMAND='pytest {project}/python/test'
