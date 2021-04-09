769f726d7b780fac55b9c9cde00cfd0a9576f111
Tom Vander Aa <vanderaa@imec.be>
2021-04-09 12:03:48 +0200

    ENH: jaak data test: large/small dataset

--- data/jaak/test_py.py
+++ data/jaak/test_py.py
@@ -5,17 +5,28 @@ import smurff.matrix_io as mio
 import numpy as np
 from time import time
 
-#load data
-ic50_train = mio.read_matrix("chembl-IC50-346targets-100compounds-train.sdm")
-ic50_test = mio.read_matrix("chembl-IC50-346targets-100compounds-test.sdm")
-#feat = mio.read_matrix("chembl-IC50-100compounds-feat-dense.ddm")
-feat = mio.read_matrix("chembl-IC50-100compounds-feat.sdm")
+DATASET_SIZE = "large"
+
+if DATASET_SIZE == "small":
+    #small data
+    ic50_train = mio.read_matrix("chembl-IC50-346targets-100compounds-train.sdm")
+    ic50_test = mio.read_matrix("chembl-IC50-346targets-100compounds-test.sdm")
+    #feat = mio.read_matrix("chembl-IC50-100compounds-feat-dense.ddm")
+    feat = mio.read_matrix("chembl-IC50-100compounds-feat.sdm")
+elif DATASET_SIZE == "large":
+    #larger data
+    ic50_train = mio.read_matrix("chembl-IC50-346targets-train.sdm")
+    ic50_test = mio.read_matrix("chembl-IC50-346targets-test.sdm")
+    #feat = mio.read_matrix("chembl-IC50-compound-feat-dense.ddm")
+    feat = mio.read_matrix("chembl-IC50-compound-feat.sdm")
+else:
+    raise ValueError("DATASET_SIZE should be 'large' or 'small'")
 
 ic50_threshold = 6.
 
 trainSession = smurff.TrainSession(
                             verbose = 1,
-                            priors = ['macau', 'normal'],
+                            priors = ['macauone', 'normalone'],
                             num_latent=32,
                             num_threads=1,
                             seed=1234,
@@ -26,7 +37,7 @@ trainSession = smurff.TrainSession(
 
 ## using activity threshold pIC50 > 6. to binarize train data
 trainSession.addTrainAndTest(ic50_train, ic50_test)
-trainSession.addSideInfo(0, feat, noise=smurff.SampledNoise(), direct=True)
+trainSession.addSideInfo(0, feat, noise=smurff.SampledNoise(), direct=False)
 
 start = time()
 predictions = trainSession.run()
