271f0f31d70609067bdf3399f289dac726ea03f9
Tom Vander Aa <vanderaa@imec.be>
2024-06-13 15:29:06 +0200

    conda: cmake args from variant

--- conda-recipes/smurff/build.sh
+++ conda-recipes/smurff/build.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-export CMAKE_ARGS="-DCMAKE_INSTALL_PREFIX=$PREFIX -DENABLE_MPI=OFF"
+if [ $blas_impl == "mkl" ]
+then
+    SKBUILD_CMAKE_ARGS="-DENABLE_MKL=ON"
+elif [ $blas_impl == "openblas" ]
+then
+    SKBUILD_CMAKE_ARGS="-DENABLE_OPENBLAS=ON"
+fi
 
-echo "extra CMAKE_ARGS: $SKBUILD_CMAKE_ARGS"
+export SKBUILD_CMAKE_ARGS
 
-$PYTHON -m pip install . --no-deps -vv
+$PYTHON -m pip install . \
+    $EXTRA_CMAKE_ARGS \
+    --no-deps -vv
