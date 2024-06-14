b598d006c8622be8fa70d481d8479bbf7f949a14
Tom Vander Aa <vanderaa@imec.be>
2024-06-14 10:09:59 +0200

    conda: blas_impl

--- conda-recipes/smurff/build.sh
+++ conda-recipes/smurff/build.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-if [ $blas_impl == "mkl" ]
+if [ "$blas_impl" == "mkl" ]
 then
     SKBUILD_CMAKE_ARGS="-DENABLE_MKL=ON"
-elif [ $blas_impl == "openblas" ]
-then
-    SKBUILD_CMAKE_ARGS="-DENABLE_OPENBLAS=ON"
 else
-    echo "Unknown BLAS impl: \"$blas_impl\""
-    exit -1
+    SKBUILD_CMAKE_ARGS="-DENABLE_OPENBLAS=ON"
 fi
 
 echo "extra CMAKE_ARGS: $SKBUILD_CMAKE_ARGS"
