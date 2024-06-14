cc94e85c673701482b49f0f71e4f465646574711
Tom Vander Aa <vanderaa@imec.be>
2024-06-14 10:01:07 +0200

    conda: blas_impl

--- conda-recipes/smurff/build.sh
+++ conda-recipes/smurff/build.sh
@@ -6,10 +6,13 @@ then
 elif [ $blas_impl == "openblas" ]
 then
     SKBUILD_CMAKE_ARGS="-DENABLE_OPENBLAS=ON"
+else
+    echo "Unknown BLAS impl: \"$blas_impl\""
+    exit -1
 fi
+
+echo "extra CMAKE_ARGS: $SKBUILD_CMAKE_ARGS"
 
 export SKBUILD_CMAKE_ARGS
 
-$PYTHON -m pip install . \
-    $EXTRA_CMAKE_ARGS \
-    --no-deps -vv
+$PYTHON -m pip install . --no-deps -vv
