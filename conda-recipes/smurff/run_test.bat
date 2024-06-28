8f0a535aca2bbbc56c43bf5333c3b1427acae49e
Tom Vander Aa <vanderaa@imec.be>
2024-06-28 10:28:30 +0200

    conda: display verbose version info as first test

--- conda-recipes/smurff/run_test.bat
+++ conda-recipes/smurff/run_test.bat
@@ -1,3 +1,5 @@
+%CONDA_PREFIX%\Scripts\smurff --version --verbose=1
+if errorlevel 1 exit 1
 %CONDA_PREFIX%\Scripts\smurff --bist ~[random]
 if errorlevel 1 exit 1
 %PYTHON% -m pytest -n auto -v
--- conda-recipes/smurff/run_test.sh
+++ conda-recipes/smurff/run_test.sh
@@ -1,2 +1,3 @@
+$PREFIX/bin/smurff --verbose=1 --version
 $PREFIX/bin/smurff --bist
 $PYTHON -m pytest -n auto -v python/test
