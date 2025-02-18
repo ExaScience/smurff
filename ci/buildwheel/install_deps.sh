cfc943bcd249de3c8163f263dc3d62d70f48bc9b
Tom Vander Aa <vanderaa@imec.be>
2025-02-18 14:40:40 +0100

    vcpgkg: for windows and macos

--- ci/buildwheel/install_deps.sh
+++ ci/buildwheel/install_deps.sh
@@ -3,12 +3,5 @@
 set -ex
 
 SCRIPT_DIR=$(dirname ${BASH_SOURCE[0]})
-cd "$SCRIPT_DIR"
 
-echo "MACOSX_DEPLOYMENT_TARGET: [$MACOSX_DEPLOYMENT_TARGET]"
-echo "PWD: [$PWD]"
-
-brew install  --formulae eigen ../highfive.rb catch2
-brew uninstall --ignore-dependencies hdf5@1.10
-
-./install_hdf5.sh
+./vcpkg/bootstrap-vcpkg.sh
