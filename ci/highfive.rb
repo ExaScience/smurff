b3ae4e239a92ea20ea9e44c23b36cd86af69ae34
Tom Vander Aa <vanderaa@imec.be>
2026-01-15 17:04:50 +0100

    HighFive: use v2.10.1

--- ci/highfive.rb
+++ ci/highfive.rb
@@ -4,7 +4,7 @@
 class Highfive < Formula
   desc "HighFive - Header-only C++ HDF5 interface"
   homepage "https://bluebrain.github.io/HighFive/"
-  url "https://github.com/BlueBrain/HighFive/archive/v2.2.2.tar.gz"
+  url "https://github.com/BlueBrain/HighFive/archive/v2.10.1.tar.gz"
   sha256 "5bfb356705c6feb9d46a0507573028b289083ec4b4607a6f36187cb916f085a7"
 
   depends_on "cmake" => :build
